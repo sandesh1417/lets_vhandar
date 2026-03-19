996a37e0cac6d8659fbd774358bb79d77d3c25de
sandesh1417 <sandeshshrestha017@gmail.com>
2026-03-19 15:52:49 +0545

    FIX :: kts issue fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,9 +15,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -36,6 +34,12 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
