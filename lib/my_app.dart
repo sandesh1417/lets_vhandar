6c3a9f888b9f49cdf2a8aba867b2d81a9427ae1b
sandesh1417 <sandeshshrestha017@gmail.com>
2026-05-14 12:29:20 +0545

    FIX :: nav bar fix

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             theme: AppTheme.lightTheme,
             darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.light,
           ),
         );
       },
