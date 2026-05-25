be8aff8b53cf8f08375d1c860f6c7eb11fe33ebb
sandesh1417 <sandeshshrestha017@gmail.com>
2026-05-25 16:50:17 +0545

    FIX :: app id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 android {
-    namespace = "com.vhandar.app"
+    namespace = "com.vhandar"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -44,7 +44,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.vhandar.app"
+        applicationId = "com.vhandar"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/vhandar/MainActivity.kt
+++ android/app/src/main/kotlin/com/vhandar/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.vhandar.app
+package com.vhandar
 
 import io.flutter.embedding.android.FlutterActivity
 
