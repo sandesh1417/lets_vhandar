a355debb0bc1a4e04f1b0bfaac42fcaaa1e70731
sandesh1417 <sandeshshrestha017@gmail.com>
2026-06-08 15:27:05 +0545

    FEAT :: ios

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
+      case TargetPlatform.iOS:
+        return ios;
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -19,5 +22,14 @@ class DefaultFirebaseOptions {
     messagingSenderId: '683559346199',
     projectId: 'vhandar-950a3',
     storageBucket: 'vhandar-950a3.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyBXdcluzKIe35Re82iMTaR8p35_m8nSGXM',
+    appId: '1:683559346199:ios:0397944a12696f0a82fc52',
+    messagingSenderId: '683559346199',
+    projectId: 'vhandar-950a3',
+    storageBucket: 'vhandar-950a3.firebasestorage.app',
+    iosBundleId: 'com.vhandar.app',
   );
 }
