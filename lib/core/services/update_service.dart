836444f9061c5782c24fce7c9acbd954b7b66b6f
sandesh1417 <sandeshshrestha017@gmail.com>
2026-06-09 15:05:19 +0545

    FIX :: time

--- lib/core/services/update_service.dart
+++ lib/core/services/update_service.dart
@@ -19,7 +19,7 @@ class UpdateService {
 
       await remoteConfig.setConfigSettings(RemoteConfigSettings(
         fetchTimeout: const Duration(seconds: 10),
-        minimumFetchInterval: const Duration(minutes: 10),
+        minimumFetchInterval: const Duration(minutes: 5),
       ));
 
       await remoteConfig.setDefaults(const {
