0260b119dc9d72fb2976f3979fad8eaa22728c35
sandesh1417 <sandeshshrestha017@gmail.com>
2026-06-09 15:05:10 +0545

    FIX :: min 10 set in upgrade

--- lib/core/services/update_service.dart
+++ lib/core/services/update_service.dart
@@ -19,7 +19,7 @@ class UpdateService {
 
       await remoteConfig.setConfigSettings(RemoteConfigSettings(
         fetchTimeout: const Duration(seconds: 10),
-        minimumFetchInterval: const Duration(hours: 1),
+        minimumFetchInterval: const Duration(minutes: 10),
       ));
 
       await remoteConfig.setDefaults(const {
